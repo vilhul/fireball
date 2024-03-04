757d869e49472d4cb416852152d908899d3ee441
vilhul <vilhul222@edu.linkoping.se>
2024-03-04 15:59:05 +0100

    init
    tror man skriver så lol

---
+++ firball/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+        }
+    }
+}
