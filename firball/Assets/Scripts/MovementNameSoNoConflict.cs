889a499b3be22eaf6a3090a92f8999c5c6dfd872
Snuskis0 <simosk685@edu.linkoping.se>
2024-03-05 15:37:40 +0100

    testning
    Klar med testning för dagen, ses imorn

---
+++ firball/Assets/Scripts/MovementNameSoNoConflict.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementNameSoNoConflict : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
