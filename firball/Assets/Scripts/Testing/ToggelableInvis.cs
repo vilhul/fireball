20a682c0717a1b5633e48aff73413aafe01d7448
Snuskis0 <simosk685@edu.linkoping.se>
2024-03-06 13:54:51 +0100

    Klar för dagen
    testat göra SpeechBubbles, ser helt okej ut (Paxlax fixar). Ska kolla ScriptableObjects nästa gång för att göra det ÄNNU bättre. Har temporärt skapat en prefab för det jag har gjort än så länge

--- firball/Assets/Scripts/Testing/ToggelableInvis.cs
+++ firball/Assets/Scripts/Testing/ToggelableInvis.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestingForSpeechBubble : MonoBehaviour
+public class ToggelableInvis : MonoBehaviour
 {
-
-
-
     // Start is called before the first frame update
     void Start()
     {
