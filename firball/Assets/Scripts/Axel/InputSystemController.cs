50649265b3f8dec027a40a4f3d5dde7d5bd4256e
AxeBjo057 <axebjo057@edu.linkoping.se>
2024-03-07 12:39:39 +0100

    GroundCheck
    GroundCheck ska fungera, men gör icke det, måste fixa vidare men hinner inte nu

--- firball/Assets/Scripts/Axel/InputSystemController.cs
+++ firball/Assets/Scripts/Axel/InputSystemController.cs
@@ -1,30 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputSystemController : MonoBehaviour
 {
     public Rigidbody2D rb;
-    private float playerMovementDirection;
 
-    [SerializeField]
+
+    [Header("Movement")]
     private float playerMovementSpeed = 5f;
-    [SerializeField]
+    private float playerMovementDirection;
+    [Header("Jumping")]
     private float playerJumpStrength = 10f;
-    
+
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.5f);
+    public LayerMask groundLayer;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(playerMovementDirection * playerMovementSpeed, rb.velocity.y);
-       
+
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -34,14 +42,34 @@ public class InputSystemController : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-
-        if (context.performed)
+        if (isGrounded())
         {
-            if (rb.velocity.y == 0)
+            if (context.performed)
             {
-                rb.AddForce(new Vector2(0, playerJumpStrength), ForceMode2D.Impulse);
+                //hold down space => full jump power
+                rb.velocity = new Vector2(rb.velocity.x, playerJumpStrength);
             }
+            else if (context.canceled)
+            {
+                //tap space => half jump power
+                rb.velocity = new Vector2(rb.velocity.x, playerJumpStrength * 0.5f);
+            }
+        }
     }
 
-}
+    private bool isGrounded()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer));
+        {
+            return true;
+        }
+        return false;
+        
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
 }
