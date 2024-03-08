f3e05ee0fd0cbd623fc01610544649380dcf51aa
Cassav270 <cassav270@edu.linkoping.se>
2024-03-08 09:12:02 +0100

    Enemy kan hoppa och gå
    Gjorde att Enemy kan hoppa och gå i båda hållen. Nu ska jag göra så att den går utan att man trycker på en knapp

--- firball/Assets/Scripts/EnemyBasicMovement.cs
+++ firball/Assets/Scripts/EnemyBasicMovement.cs
@@ -2,18 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBasicMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    private float horizontal; 
+    private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform Enemy;
+    [SerializeField] private LayerMask groundLayer;
 
-    // Update is called once per frame
-    private void Update() 
+    void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            Debug.Log("gubben ska hoppa");
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(Enemy.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
 
