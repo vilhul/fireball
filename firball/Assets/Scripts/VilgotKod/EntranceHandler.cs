24e8018de0c5bc0514a05fd4b5f1131ddf55ab58
vilhul <vilhul222@edu.linkoping.se>
2024-03-08 16:27:57 +0100

    lite med scenes

--- firball/Assets/Scripts/VilgotKod/EntranceHandler.cs
+++ firball/Assets/Scripts/VilgotKod/EntranceHandler.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EntranceHandler : MonoBehaviour
 {
 
     [SerializeField] private GameObject player;
     [SerializeField] private EntranceDataSO entranceDataSO;
+
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Debug.Log(player);
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
