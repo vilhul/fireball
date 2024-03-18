0632cea2377088a818d9fb370ce0f434b7fb240a
AxeBjo057 <axebjo057@edu.linkoping.se>
2024-03-18 11:18:23 +0100

    Jobbar ui
    metoderkörs när knappar klickas på!

--- firball/Assets/Scripts/Axel/MainMenu.cs
+++ firball/Assets/Scripts/Axel/MainMenu.cs
@@ -6,38 +6,54 @@ using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private UIDocument uiDoc;
+    private VisualElement rootEl;
+    private Button startBtn;
+    private Button optionsBtn;
+    private Button quitBtn;
     private VisualElement animatedFireball;
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnEnable()
     {
-        var root = GetComponent<UIDocument>().rootVisualElement;
+        rootEl = uiDoc.rootVisualElement;
 
-        animatedFireball = root.Q<VisualElement>("Fireball");
+        startBtn = rootEl.Query<Button>("start");
+        optionsBtn = rootEl.Query<Button>("options");
+        quitBtn = rootEl.Query<Button>("quit");
+        //animatedFireball = rootEl.Query<VisualElement>("fireball");
 
-        
+        //animatedFireball.RegisterCallback<TransitionEndEvent>( (evt) => {
+        //    animatedFireball.ToggleInClassList("object-down");
+        //});
+
+        startBtn.RegisterCallback<ClickEvent>( (evt) =>
+        {
+            startBtnClickedMethod();
+        });
+
+        optionsBtn.RegisterCallback<ClickEvent>((evt) =>
+        {
+            optionsBtnClickedMethod();
+        });
+
+        quitBtn.RegisterCallback<ClickEvent>((evt) =>
+        {
+            quitBtnClickedMethod();
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    private void startBtnClickedMethod()
     {
-        
-    }
-    private void animateFireball()
-    {
-        animatedFireball.ToggleInClassList("object-up");
-        animatedFireball.RegisterCallback<TransitionEndEvent>(animateFireballBack);
+        Debug.Log("startknapp klickad");
     }
 
-    private void animateFireballBack(TransitionEndEvent evt)
+    private void optionsBtnClickedMethod()
     {
-        Debug.Log("funktion kallad");
-        animatedFireball.ToggleInClassList("object-up");
-        animatedFireball.RegisterCallback<TransitionEndEvent>(animateFireball2);
+        Debug.Log("inställningsknapp klickad");
     }
 
-    private void animateFireball2(TransitionEndEvent evt)
+    private void quitBtnClickedMethod()
     {
-        animatedFireball.ToggleInClassList("object-up");
-        animatedFireball.RegisterCallback<TransitionEndEvent>(animateFireballBack);
+        Debug.Log("lämnaknapp klickad");
     }
 }
