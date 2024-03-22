278940a1a6206ed790444031a9785164ce16c69a
AxeBjo057 <axebjo057@edu.linkoping.se>
2024-03-22 09:07:44 +0100

    Main Menu
    Den är gjord, men ser inte så bra ut än, dock är det fritt fram att göra så att funktioner körs till de olika knapparna

--- firball/Assets/Scripts/Axel/MainMenu.cs
+++ firball/Assets/Scripts/Axel/MainMenu.cs
@@ -38,25 +38,27 @@ public class MainMenu : MonoBehaviour
             quitBtnClickedMethod();
         });
 
-        //skapar flera fireballs
+        //skapar flera fireballs med random storlek och random position vart de spawnar horisontellt
         for (int i = 0; i < 5; i++) {
         Debug.Log("skapade meteor");
         meteor = new VisualElement();
         rootEl.Add(meteor);
         meteor.AddToClassList("meteor");
         System.Random random = new System.Random();
-        int randomNum = random.Next(200, 701);
+        int randomNum = random.Next(200, 401);
         meteor.style.height = randomNum;
         meteor.style.width = randomNum;
         meteor.style.top = -randomNum;
-        meteor.style.right = -randomNum;
+        int randomPos = random.Next(randomNum, 1920-randomNum);
+        meteor.style.right = -randomPos;
         meteor.ToggleInClassList("animate-meteor");
         }
 
+        // skapar lista av de och loopar genom den så att de animeras
         var meteors = rootEl.Query(className: "meteor").ToList();
-
         foreach (var meteor in meteors)
         {
+            //random när de ska swisha förbi på skärmen
             System.Random random = new System.Random();
             int randomNum = random.Next(1, 400);
             Debug.Log("nåt");
@@ -64,8 +66,15 @@ public class MainMenu : MonoBehaviour
             {
                 
                 meteor.ToggleInClassList("animate-meteor");
-                Debug.Log("animeras");
+                
             }).Every(4000 + randomNum * 4);
+
+            //ändrar sprite varje 0.5 sekunder
+            rootEl.schedule.Execute(() =>
+            {
+                meteor.ToggleInClassList("switch-fireball-image");
+               
+            }).Every(20);
 
         }
 
@@ -85,5 +94,8 @@ public class MainMenu : MonoBehaviour
     private void quitBtnClickedMethod()
     {
         Debug.Log("lämnaknapp klickad");
+        Application.Quit();
+        //koden nedan är bara för att quitknappen ska fungera i editorn
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
