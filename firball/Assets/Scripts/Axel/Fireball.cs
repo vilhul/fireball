c0b046cb584b420b9a32b4614a5cc609e920fe1d
AxeBjo057 <axebjo057@edu.linkoping.se>
2024-04-29 16:43:44 +0200

    Fireball update
    Eldbollen spawnar och åker åt rätt hål med rätt sprite-riktning, men den spawnar ibland 2 gånger vilket måste lösas.
    
    (jag lär fråga ludvig)

--- firball/Assets/Scripts/Axel/Fireball.cs
+++ firball/Assets/Scripts/Axel/Fireball.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 [CreateAssetMenu]
 public class Fireball : Ability
 {
@@ -9,13 +10,25 @@ public class Fireball : Ability
     
     public override void Activate(GameObject parent)
     {
-        GameObject fireball = Instantiate(fireballProjectile, parent.transform.position, Quaternion.identity);
-
+        GameObject fireball = Instantiate(fireballProjectile, parent.transform.position, Quaternion.identity); ;
+        InputSystemController movement = parent.GetComponent<InputSystemController>();
+        bool isFacingRight = movement.GetIsFacingRight();
         Rigidbody2D rb = fireball.GetComponent<Rigidbody2D>();
-
+        
         if (rb != null )
         {
-            rb.velocity = parent.transform.right * fireballSpeed;
+            if (isFacingRight)
+            {
+                rb.velocity = parent.transform.right * fireballSpeed;
+                fireball.transform.position = new Vector3(fireball.transform.position.x + 1f, fireball.transform.position.y, fireball.transform.position.z);
+                
+            }
+            else
+            {
+                rb.velocity = -parent.transform.right * fireballSpeed;
+                fireball.transform.position = new Vector3(fireball.transform.position.x - 1f, fireball.transform.position.y, fireball.transform.position.z);
+                fireball.transform.rotation = Quaternion.Euler(0,180,0);
+            }
         }else
         {
             Debug.LogError("Fireball prefab saknar Rigidbody2D-komponent!");
