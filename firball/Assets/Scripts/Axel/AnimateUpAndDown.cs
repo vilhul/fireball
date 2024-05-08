e348c852224a89ba646768d923328dbdfe75810e
AxeBjo057 <axebjo057@edu.linkoping.se>
2024-05-08 09:30:44 +0200

    Ny maincharacter anim
    japp

--- firball/Assets/Scripts/Axel/AnimateUpAndDown.cs
+++ firball/Assets/Scripts/Axel/AnimateUpAndDown.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class AnimateUpAndDown : MonoBehaviour
 {
     float amp;
+    float speed;
     private void Awake()
     {
-        amp = Random.Range(0.001f, 0.005f);
+        amp = Random.Range(0.001f, 0.003f);
+        speed = Random.Range(8f, 15f);
     }
     
     private void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y + Mathf.Sin(15*Time.time) * amp, 0);    
+        transform.position = new Vector3(transform.position.x, transform.position.y + Mathf.Sin(speed*Time.time) * amp, 0);    
     }
 }
