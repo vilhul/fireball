30df50b012d698c22c15b28d84bd29b30f68e172
vilhul <vilhul222@edu.linkoping.se>
2024-05-21 13:58:27 +0200

    började fixa allt som simon fuckat upp

---
+++ firball/Assets/LadderLogic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderLogic : MonoBehaviour
+{
+    InputSystemController playerMovement;
+
+    private void Awake()
+    {
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<InputSystemController>();
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            playerMovement.PlayerRb.velocity = new Vector2(playerMovement.PlayerRb.velocity.x, 10f);
+        }
+    }
+}
