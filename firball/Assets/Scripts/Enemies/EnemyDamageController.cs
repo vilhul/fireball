d4d0c2af07490d294e930185598a361ac151cef9
AxeBjo057 <axebjo057@edu.linkoping.se>
2024-05-22 13:16:34 +0200

    Enemy tar damage
    påbörjat enemy kan ta damage

---
+++ firball/Assets/Scripts/Enemies/EnemyDamageController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamageController : MonoBehaviour
+{
+    public float hp = 100f;
+    public float maxHp = 100f;
+
+    void Update()
+    {
+        if ((hp <= 0f))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Take10Damage()
+    {
+        hp -= 10f;
+    }
+
+    public void Take25Damage()
+    {
+        hp -= 25f;
+    }
+
+    public void Take50Damage()
+    {
+        hp -= 50f;
+    }
+
+    public void DIE()
+    {
+        hp = 0f;
+    }
+}
